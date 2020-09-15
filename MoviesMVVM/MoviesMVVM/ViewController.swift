be2ef8ee9accc470a4efe6bdc47c7f3eb2df9415
RomanEnergY <Zverik.r.s@gmail.com>
2020-09-15 07:22:28 +0300

    create project

---
+++ MoviesMVVM/MoviesMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesMVVM
+//
+//  Created by 1234 on 15.09.2020.
+//  Copyright © 2020 ZRS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
