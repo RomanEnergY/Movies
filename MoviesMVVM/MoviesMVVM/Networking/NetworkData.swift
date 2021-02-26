55b3aa272dc280bad19b996fa2431ec6c06a187d
Roman Zverik <Roman.Zverik@waveaccess.ru>
2021-02-26 19:07:38 +0300

    update APIKey api.themoviedb.org

--- MoviesMVVM/MoviesMVVM/Networking/NetworkData.swift
+++ MoviesMVVM/MoviesMVVM/Networking/NetworkData.swift
@@ -10,6 +10,6 @@ import Foundation
 
 enum NetworkData {
     static let baseURL = "https://api.themoviedb.org/3/"
-    static let APIKey = "794a530f52335c4139ed740dcd55a4ac"
+    static let APIKey = "c5bcaec10176d796d5529adfa4fb2a4b"
     static let language = "ru-RU"
 }
